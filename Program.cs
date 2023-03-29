029d0ac62155f8835442adea40f7579156c62dd5
gdemartino93 <113302882+gdemartino93@users.noreply.github.com>
2023-03-29 20:49:35 +0200

    Aggiungere i file di progetto.

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿namespace c_sharp_file
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
