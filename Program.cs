60ca74fa12bac4fbcef6ad4170a46caa4fef0440
gdemartino93 <113302882+gdemartino93@users.noreply.github.com>
2023-03-29 21:01:14 +0200

    create new file with content from input if not empty

--- Program.cs
+++ Program.cs
@@ -1,10 +1,15 @@
 ﻿namespace c_sharp_file
 {
+    using System.IO;
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Console.WriteLine("Inserisci il nome del file che vuoi creare");
+            string nomeFile = Console.ReadLine();
+            Console.WriteLine("Inserisci il contenuto del file");
+            string? contenuto = Console.ReadLine();
+            File.WriteAllText($"{nomeFile}.txt", contenuto == "" ? "File vuoto" : contenuto);
         }
     }
 }
