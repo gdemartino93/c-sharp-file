d755427ddd510503fed9a3f1b33c516f3ff7083f
gdemartino93 <113302882+gdemartino93@users.noreply.github.com>
2023-03-29 21:30:09 +0200

    check if file exist and ask to user for overwrite it. options work with enums

--- Program.cs
+++ Program.cs
@@ -3,12 +3,32 @@
     using System.IO;
     internal class Program
     {
+        enum Opzioni
+        {
+            No,
+            Yes
+            
+        }
         static void Main(string[] args)
         {
+  
             Console.WriteLine("Inserisci il nome del file da creare");
             string nomeFile = Console.ReadLine();
             Console.WriteLine("Inserisci il contenuto del file");
             string contenutoFile = Console.ReadLine();
+
+            string filePath = $@"c:\Users\gdema\Desktop\csharp\{nomeFile}.txt";
+
+            if (File.Exists(filePath))
+            {
+                Console.WriteLine("File già esistente. Vuoi sovrascriverlo? Si/No");
+                Opzioni risposta = (Opzioni)Enum.Parse(typeof(Opzioni), Console.ReadLine(), true);
+                if (risposta == Opzioni.No)
+                {
+                    Console.WriteLine("Il file non è stato sovrascritto.");
+                    return;
+                }
+            }
 
             File.WriteAllText($@"c:\Users\gdema\Desktop\csharp\{nomeFile}.txt",contenutoFile);
         }
